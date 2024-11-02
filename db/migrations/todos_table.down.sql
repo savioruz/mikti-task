8ee9f7b417c40e54c9d68fabb7a2616905b790ee
jakues <jakueenak@gmail.com>
2024-11-02 14:44:38 +0700

    Ref: change migrations sql

--- db/migrations/todos_table.down.sql
+++
@@ -1,5 +0,0 @@
--- Table: public.todos
-
-DROP TABLE IF EXISTS public.todos;
-
-DROP INDEX IF EXISTS public.idx_todos_deleted_at;
--- db/migrations/users_table.down.sql
+++
@@ -1,5 +0,0 @@
--- Table: public.users
-
-DROP TABLE IF EXISTS public.users;
-
-DROP INDEX IF EXISTS public.idx_users_deleted_at;
