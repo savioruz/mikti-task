a38b799d1059aa326e1482bda2c868cae0146e8f
jakues <jakueenak@gmail.com>
2024-11-13 19:02:58 +0700

    Ref: change jwt response error

--- internal/delivery/http/middleware/auth_middleware.go
+++ internal/delivery/http/middleware/auth_middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/savioruz/mikti-task/tree/week-4/internal/domain/model"
 	"github.com/savioruz/mikti-task/tree/week-4/internal/platform/jwt"
 	"net/http"
 	"strings"
@@ -10,19 +11,23 @@ import (
 func AuthMiddleware(jwtService jwt.JWTService) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
+			errMessage := func(message string) error {
+				return echo.NewHTTPError(http.StatusUnauthorized, model.NewErrorResponse[any](http.StatusUnauthorized, message))
+			}
+
 			authHeader := c.Request().Header.Get("Authorization")
 			if authHeader == "" {
-				return echo.NewHTTPError(http.StatusUnauthorized, "Missing authorization header")
+				return errMessage("Missing authorization header")
 			}
 
 			bearerToken := strings.Split(authHeader, " ")
 			if len(bearerToken) != 2 || bearerToken[0] != "Bearer" {
-				return echo.NewHTTPError(http.StatusUnauthorized, "Invalid authorization header")
+				return errMessage("Invalid authorization header")
 			}
 
 			claims, err := jwtService.ValidateToken(bearerToken[1])
 			if err != nil {
-				return echo.NewHTTPError(http.StatusUnauthorized, "Invalid token")
+				return errMessage("Invalid token")
 			}
 
 			c.Set("user", claims)
